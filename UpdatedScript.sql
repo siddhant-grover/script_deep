98d7e7e0e5d0ece333bc27ea72604ac8a90f88df
Siddhant Grover <siddhantgroverboss@gmail.com>
2023-04-10 01:56:46 +0530

    updated

--- UpdatedScript.sql
+++ UpdatedScript.sql
@@ -1,29 +1,30 @@
+/*id primary key? in all tables*/
 CREATE TABLE `facility` (
-  `Facility_Id` varchar(10) NOT NULL,
+  `Facility_Id` int(10) NOT NULL AUTO_INCREMENT,
   `Facility_Identification_Number` varchar(10) DEFAULT NULL,
   `Facility_Name` varchar(30) DEFAULT NULL,
-  `Facility_Phone` int DEFAULT NULL,
-  `Facility_Emergency_Phone` int DEFAULT NULL,
-  `Facility_On_Call_Number` int DEFAULT NULL,
+  `Facility_Phone` varchar(15) DEFAULT NULL,
+  `Facility_Emergency_Phone` varchar(15) DEFAULT NULL,
+  `Facility_On_Call_Number` varchar(15) DEFAULT NULL,
   `Facility_Form_Details` varchar(10) DEFAULT NULL,
-  `Facility_Start_Date` date DEFAULT NULL,
+  `Facility_Start_Date` date DEFAULT NULL, 
   `Facility_End_Date` date DEFAULT NULL,
-  `Facility_Expiry_Date` time DEFAULT NULL,
+  `Facility_Expiry_Date` date DEFAULT NULL,/* date or datetime*/
   `Facility_Street` varchar(25) DEFAULT NULL,
   `Facility_City` varchar(10) DEFAULT NULL,
   `Facility_State` varchar(10) DEFAULT NULL,
-  `Facility_Zip` int DEFAULT NULL,
+  `Facility_Zip` varchar(20) DEFAULT NULL,
   `Facility_Mailing_Address` varchar(30) DEFAULT NULL,
   `Facility_Latitude` varchar(10) DEFAULT NULL,
   `Facility_Longitude` varchar(10) DEFAULT NULL,
-  `Facility_24_7` int DEFAULT '0',
+  `Facility_24_7` boolean DEFAULT 0,
   `Facility_Capacity` int DEFAULT NULL,
   `Facility_Email` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`Facility_Id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `services` (
-  `Service_Id` varchar(10) NOT NULL,
+  `Service_Id` int(10) NOT NULL AUTO_INCREMENT,
   `Service_Name` varchar(30) DEFAULT NULL,
   `Service_Category` varchar(30) DEFAULT NULL,
   `Service_Description` varchar(50) DEFAULT NULL,
@@ -32,21 +33,37 @@ CREATE TABLE `services` (
 
 
 CREATE TABLE `facility_dynamic_attributes` (
-  `Tag_Id` varchar(10) DEFAULT NULL,
+  `Tag_Id` int(10) DEFAULT NULL, /*not a PK?*/
   `Tag_Name` varchar(30) DEFAULT NULL,
   `Tag_Description` varchar(50) DEFAULT NULL,
-  `Facility_Id` varchar(10) DEFAULT NULL,
+  `Facility_Id` int(10) DEFAULT NULL,
   KEY `Facility_Id` (`Facility_Id`),
   CONSTRAINT `facility_dynamic_attributes_ibfk_1` FOREIGN KEY (`Facility_Id`) REFERENCES `facility` (`Facility_Id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE `facility_forms` (
-  `Form_Id` varchar(10) NOT NULL,
+CREATE TABLE `facility_services` (
+  `Facility_Service_Id` int(10) NOT NULL AUTO_INCREMENT,
+  `Service_Start_Date` date DEFAULT NULL,
+  `Service_End_Date` date DEFAULT NULL,
+  `Service_Bed_Availability` int DEFAULT NULL,
+  `Service_Seat_Availability` int DEFAULT NULL,
+  `Service_Room_Availability` int DEFAULT NULL,
+  `Facility_Id` int(10) DEFAULT NULL,
+  `Services_Id` int(10) DEFAULT NULL,
+  PRIMARY KEY (`Facility_Service_Id`),
+  KEY `Facility_Id` (`Facility_Id`),
+  KEY `Services_Id` (`Services_Id`),
+  CONSTRAINT `facility_services_ibfk_1` FOREIGN KEY (`Facility_Id`) REFERENCES `facility` (`Facility_Id`),
+  CONSTRAINT `facility_services_ibfk_2` FOREIGN KEY (`Services_Id`) REFERENCES `services` (`Service_Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `facility_forms` ( /*need to make some filds not null*/
+  `Form_Id` int(10) NOT NULL AUTO_INCREMENT,
   `Form_Name` varchar(30) DEFAULT NULL,
   `Form_Description` varchar(50) DEFAULT NULL,
   `Form_Path` varchar(50) DEFAULT NULL,
-  `Facility_Id` varchar(10) DEFAULT NULL,
-  `Facility_Service_Id` varchar(10) DEFAULT NULL,
+  `Facility_Id` int(10) DEFAULT NULL,
+  `Facility_Service_Id` int(10) DEFAULT NULL,
   PRIMARY KEY (`Form_Id`),
   KEY `Facility_Id` (`Facility_Id`),
   KEY `Facility_Service_Id` (`Facility_Service_Id`),
@@ -56,16 +73,16 @@ CREATE TABLE `facility_forms` (
 
 
 CREATE TABLE `facility_key_providers` (
-  `Provider_Id` varchar(10) NOT NULL,
+  `Provider_Id` int(10) NOT NULL AUTO_INCREMENT,
   `Provider_First_Name` varchar(30) DEFAULT NULL,
   `Provider_Middle_Name` varchar(30) DEFAULT NULL,
   `Provider_Last_Name` varchar(30) DEFAULT NULL,
   `Provider_Specialization` varchar(30) DEFAULT NULL,
-  `Provider_Working_Hours` time DEFAULT NULL,
+  `Provider_Working_Hours` time DEFAULT NULL,/*time? or int*/
   `Provider_Type` char(10) DEFAULT NULL,
-  `Provider_Phone_Number` int DEFAULT NULL,
-  `Facility_Id` varchar(10) DEFAULT NULL,
-  `Facility_Service_Id` varchar(10) DEFAULT NULL,
+  `Provider_Phone_Number` varchar(15) DEFAULT NULL,
+  `Facility_Id` int(10) DEFAULT NULL,
+  `Facility_Service_Id` int(10) DEFAULT NULL,
   PRIMARY KEY (`Provider_Id`),
   KEY `Facility_Id` (`Facility_Id`),
   KEY `Facility_Service_Id` (`Facility_Service_Id`),
@@ -78,35 +95,20 @@ CREATE TABLE `facility_opening_hours` (
   `Day` char(10) DEFAULT NULL,
   `Opening_Time` time DEFAULT NULL,
   `Closing_Time` time DEFAULT NULL,
-  `Facility_Id` varchar(10) DEFAULT NULL,
+  `Facility_Id` int(10) DEFAULT NULL,
   KEY `Facility_Id` (`Facility_Id`),
   CONSTRAINT `facility_opening_hours_ibfk_1` FOREIGN KEY (`Facility_Id`) REFERENCES `facility` (`Facility_Id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 
 CREATE TABLE `facility_restrictions` (
-  `Restriction_Id` varchar(10) DEFAULT NULL,
+  `Restriction_Id` int(10) DEFAULT NULL, /*not a PK?*/
   `Restriction_Name` varchar(30) DEFAULT NULL,
   `Restriction_Description` varchar(50) DEFAULT NULL,
-  `Facility_Id` varchar(10) DEFAULT NULL,
+  `Facility_Id` int(10) DEFAULT NULL,
   KEY `Facility_Id` (`Facility_Id`),
   CONSTRAINT `facility_restrictions_ibfk_1` FOREIGN KEY (`Facility_Id`) REFERENCES `facility` (`Facility_Id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 
-CREATE TABLE `facility_services` (
-  `Facility_Service_Id` varchar(10) NOT NULL,
-  `Service_Start_Date` time DEFAULT NULL,
-  `Service_End_Date` date DEFAULT NULL,
-  `Service_Bed_Availability` int DEFAULT NULL,
-  `Service_Seat_Availability` int DEFAULT NULL,
-  `Service_Room_Availability` int DEFAULT NULL,
-  `Facility_Id` varchar(10) DEFAULT NULL,
-  `Services_Id` varchar(10) DEFAULT NULL,
-  PRIMARY KEY (`Facility_Service_Id`),
-  KEY `Facility_Id` (`Facility_Id`),
-  KEY `Services_Id` (`Services_Id`),
-  CONSTRAINT `facility_services_ibfk_1` FOREIGN KEY (`Facility_Id`) REFERENCES `facility` (`Facility_Id`),
-  CONSTRAINT `facility_services_ibfk_2` FOREIGN KEY (`Services_Id`) REFERENCES `services` (`Service_Id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
